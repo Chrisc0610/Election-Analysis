00fa7e6fc43ea631f5df4af404b417f90bcc3eee
Chris Cardenas <c2christian.cardenas@gmail.com>
2021-05-15 17:21:10 -0400

    updated file

---
+++ practice.py
@@ -0,0 +1,9 @@
+import csv
+dir(csv
+
+# The data needed
+#1.Total number of votres cast
+#2 complete list of candidates who received votes
+#3 The percentage of votes each candidate wont
+#4 Total number of votes each candidate won
+#5 the winner of the elecetion based popular vote
--- pypoll.py
+++ pypoll.py
@@ -1,18 +1,77 @@
 import csv
 import os
+
 # Assign a variable for the file to load and the path.
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 # Using the with statement open the file as a text file.
+
+# 1 initialize a total vote counter.
+total_votes = 0
+
+# candidate votes
+candidate_options = []
+
+# create dictionary
+candidate_votes = {}
+
+# winning candidate and winning count tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+
 with open(file_to_load) as election_data:
-    #To do: read and analyze the data here.
+
+    # To do: read and analyze the data here.
     file_reader = csv.reader(election_data)
-    # Print the header row.
+
+    # read the header row.
     headers = next(file_reader)
-    print(headers)
-    
+
     # Print each row in the CSV file.
-    #for row in file_reader:
-     #   print(row)
-        
+    for row in file_reader:
+        # 2. add to the total vote count.
+        total_votes += 1
+
+        # print the candidate name from each row
+        candidate_name = row[2]
+
+        # if the candidate does not match any existing candidate
+        if candidate_name not in candidate_options:
+            # Add the candidate name to the candidate list
+            candidate_options.append(candidate_name)
+
+            # track the votes
+            candidate_votes[candidate_name] = 0
+        # add votes
+        candidate_votes[candidate_name] += 1
+    # iterrates through list
+    for candidate_name in candidate_votes:
+        # retrieve vote count of candidate
+        votes = candidate_votes[candidate_name]
+        # calculate the percentage of votes
+        vote_percentage = float(votes) / float(total_votes) * 100
+        # print candidate name and percentage of votes
+        # Determine if votes are greater than the winning count.
+        # 1. Determine if the votes are greater than the winning count.
+        # To do: print out each candidate's name, vote count, and percentage of votes to the terminal.
+        print(f"{candidate_name}: {vote_percentage:.2f}% ({votes:,})\n")
+
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            # 2. If true then set winning_count = votes and winning_percent =
+            # vote_percentage.
+            winning_count = votes
+            winning_percentage = vote_percentage
+            # 3. Set the winning_candidate equal to the candidate's name.
+            winning_candidate = candidate_name
+    winning_candidate_summary = (
+        f"--------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"--------------------\n"
+    )
+    # 3. print the total votes
+    print(winning_candidate_summary)
