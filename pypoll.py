d441ee153c491ef956da1fa9ad293ecb149120aa
Chris Cardenas <c2christian.cardenas@gmail.com>
2021-05-15 13:52:11 -0400

    practice files

---
+++ pypoll.py
@@ -0,0 +1,18 @@
+import csv
+import os
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+# Using the with statement open the file as a text file.
+with open(file_to_load) as election_data:
+    #To do: read and analyze the data here.
+    file_reader = csv.reader(election_data)
+    # Print the header row.
+    headers = next(file_reader)
+    print(headers)
+    
+    # Print each row in the CSV file.
+    #for row in file_reader:
+     #   print(row)
+        
